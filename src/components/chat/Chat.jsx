e772e65057099215abb4fb09ec56b17299d5fdbc
diegoverdan <diegoverdan@gmail.com>
2024-04-11 12:56:58 -0300

    ajustes na section center

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import EmojiPicker from "emoji-picker-react";
+import React, { useState } from "react";
 import "./chat.css";
 
 function Chat() {
+  const [emoji, setEmoji] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmojiClick = (e) => {
+    setText((prev) => prev + e.emoji);
+    setEmoji(false);
+  };
+
   return (
     <div className="chat">
       <div className="top">
@@ -18,16 +27,102 @@ function Chat() {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+              dolorem laboriosam fuga dolore nostrum recusandae error omnis
+              voluptate doloribus sunt autem corporis nihil, corrupti quia
+              accusamus nesciunt ab sed et?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img
+              src="https://hips.hearstapps.com/hmg-prod/images/nature-quotes-landscape-1648265299.jpg?crop=1.00xw:0.760xh;0,0.0587xh&resize=1200:*"
+              alt=""
+            />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+              dolorem laboriosam fuga dolore nostrum recusandae error omnis
+              voluptate doloribus sunt autem corporis nihil, corrupti quia
+              accusamus nesciunt ab sed et?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+              dolorem laboriosam fuga dolore nostrum recusandae error omnis
+              voluptate doloribus sunt autem corporis nihil, corrupti quia
+              accusamus nesciunt ab sed et?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+              dolorem laboriosam fuga dolore nostrum recusandae error omnis
+              voluptate doloribus sunt autem corporis nihil, corrupti quia
+              accusamus nesciunt ab sed et?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+              dolorem laboriosam fuga dolore nostrum recusandae error omnis
+              voluptate doloribus sunt autem corporis nihil, corrupti quia
+              accusamus nesciunt ab sed et?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+              dolorem laboriosam fuga dolore nostrum recusandae error omnis
+              voluptate doloribus sunt autem corporis nihil, corrupti quia
+              accusamus nesciunt ab sed et?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="botton">
         <div className="icons">
           <img src="./img.png" alt="" />
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input type="text" placeholder="Type a message..." />
+        <input
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          type="text"
+          placeholder="Type a message..."
+        />
         <div className="emoji">
-          <img src="./emoji.png" alt="" />
+          <img onClick={() => setEmoji(!emoji)} src="./emoji.png" alt="" />
+          <div className="picker">
+            <EmojiPicker
+              searchPlaceHolder="Search"
+              open={emoji}
+              onEmojiClick={handleEmojiClick}
+            />
+          </div>
         </div>
         <button className="sendButton">Send</button>
       </div>
